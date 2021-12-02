75f68ca60a6c483fa0ccc72e740371b6f9f65760
Raghav Gupta <raghavgupta216@gmail.com>
2021-12-02 23:45:49 +0530

    Update Minimum Cost to Connect Sticks.java

--- Minimum Cost to Connect Sticks.java
+++ Minimum Cost to Connect Sticks.java
@@ -1,3 +1,6 @@
+#problem link on leetcode is https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+#problem link on lintcode is https://www.lintcode.com/problem/1872/description 
+
 import java.util.*;
 public class Solution {
     public int MinimumCost(List<Integer> sticks) {
