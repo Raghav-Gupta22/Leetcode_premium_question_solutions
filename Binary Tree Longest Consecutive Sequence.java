954c7dfef9c45164b67b6c164ad4ca84193526e8
Raghav Gupta <raghavgupta216@gmail.com>
2022-09-07 00:50:52 +0530

    Update Binary Tree Longest Consecutive Sequence.java

--- Binary Tree Longest Consecutive Sequence.java
+++ Binary Tree Longest Consecutive Sequence.java
@@ -1,7 +1,7 @@
 // Problem link on leetcode: https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/
 // Problem link on lintcode: https://www.lintcode.com/problem/595
 
-//recursive O(N^2)
+//recursive O(N^2) time complexity
 public class Solution {
     public int longestConsecutive(TreeNode root) {
         if(root == null) return 0;
@@ -27,3 +27,25 @@ public class Solution {
     }
 }
 
+// single traversal O(N) time complexity
+public class Solution {
+    int max = Integer.MIN_VALUE;
+    public int longestConsecutive(TreeNode root) {
+        // write your code here
+        f(root, 1);
+        return max;
+    }
+    private void f(TreeNode root, int c){
+        if(root == null) return;
+
+        if(root.left != null && root.val + 1 == root.left.val){
+            f(root.left, c+1);
+        }
+        else f(root.left, 1);
+        if(root.right != null && root.val + 1 == root.right.val){
+            f(root.right, c+1);
+        }
+        else f(root.right, 1);
+        max = Math.max(max, c);
+    }
+}
